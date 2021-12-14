ae21d6257e5981b9a063947ab09fb1d3d2cd768e
Jungjinah <96034366+Jungjinah@users.noreply.github.com>
2021-12-14 17:25:58 +0900

    Add files via upload

---
+++ Nov30_1_start/src/YMain.java
@@ -0,0 +1,20 @@
+
+public class YMain {
+	public static void main(String[] args) throws InterruptedException {
+		
+		
+		
+		System.out.printf("___________________________________________________________\n");
+		System.out.printf("|집까지 걸린시간 : %d분\t\t\n", 30 );
+		System.out.printf("|나의 저녁 식사 메뉴 : %s\t\t\n", "삼겹살쌈밥!");
+		System.out.printf("|어제 저녁에 뭐했는가~? -> %s\t\t\n", "게임했습니당!");
+		System.out.printf("|취침시간은? -> 새벽 %d시%d분\t\n", 1 , 30);
+		System.out.printf("|첫날 소감은 어땠는가? -> %s\t\t\n",  "첫날부터 재밌는거 배운 느낌적인 느낌~");
+		
+		System.out.printf("|앉은지 15분만에 드는 생각은? %s\t\t\n", "생각보다 기억이 잘 나서 좋움!" );
+		System.out.printf("|오늘은 며칠인가요~? %d년%d월%d일\n", 2021, 11, 30);
+		
+		Thread.sleep(5000);
+		
+	}
+}
